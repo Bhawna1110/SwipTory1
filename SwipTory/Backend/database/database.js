5d2a2f336ea03a2e3c52d258f729ec93409483f2
Bhawna Suman <63039132+Bhawna1110@users.noreply.github.com>
2024-05-02 03:05:42 +0530

    Delete SwipTory/Backend directory

--- SwipTory/Backend/database/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.DB_URI)
-    .then(() => console.log('Connected to MongoDB Atlas'))
-    .catch(err => console.error('Error connecting to MongoDB:', err.message));
-
-
-module.exports = mongoose 
