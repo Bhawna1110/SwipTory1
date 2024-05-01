a7c4cbe19d4c21f7e07044cf23b72fdd8cb3feb6
Bhawna Suman <63039132+Bhawna1110@users.noreply.github.com>
2024-05-01 22:39:12 +0530

    Update database.js

--- SwipTory/Backend/database/database.js
+++ SwipTory/Backend/database/database.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// Connect to MongoDB
 mongoose.connect(process.env.DB_URI)
     .then(() => console.log('Connected to MongoDB Atlas'))
     .catch(err => console.error('Error connecting to MongoDB:', err.message));
